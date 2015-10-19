8fd74b732b4bd0fc659a5c66da8236e2ec3a6ab3
Aurora Morgan <aurora@aventine.se>
2015-10-19 22:06:02 +0200

    Support multiple exports at once, to speed up the process

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::fs;
 use std::env;
 
 use std::io::Write;
+use std::sync::Arc;
 
 fn main() {
   let resource_manager = engine::resource_manager::ResourceManager::default();
@@ -20,35 +21,41 @@ fn main() {
       println!("{}", sof.hulls().join("\n"));
     },
     "export" => {
-      let geometry_path = if args[2].starts_with("res:/") {
-        args[2].as_str()
-      } else {
-        match sof.load_hull(args[2].as_str()) {
-          None => {
-            println!("No such hull {}", args[2]);
-
-            return;
-          }
-          Some(s) => s
-        }.geometry_path()
-      };
-
-      let geometry = resource_manager.load(geometry_path).unwrap();
-      
-      let objs = match *geometry {
-        engine::resource_manager::Resource::Asset(ref m) => engine::exporter::obj::export(m),
-        _ => panic!("{:?} is not an asset, sorry.", geometry_path)
-      };
-
-      let dir = args[3].clone();
-      fs::create_dir_all(&dir).unwrap();
-
-      for (path, obj) in objs {
-        let mut f = fs::File::create(dir.clone() + path.as_str()).unwrap();
-
-        f.write_all(obj.as_bytes()).unwrap();
+      for i in 3 .. args.len() {
+        export(&sof, resource_manager.clone(), &args[i], &args[2]);
       }
     }
     cmd => panic!("Unknown command {}", cmd)
+  }
+}
+
+fn export(sof: &engine::resource_manager::sof::Manager, resource_manager: Arc<engine::resource_manager::ResourceManager>, arg: &String, destination: &String) {
+  let geometry_path = if arg.starts_with("res:/") {
+    arg.as_str()
+  } else {
+    match sof.load_hull(arg.as_str()) {
+      None => {
+        println!("No such hull {}", arg);
+
+        return;
+      }
+      Some(s) => s
+    }.geometry_path()
+  };
+
+  let geometry = resource_manager.load(geometry_path).unwrap();
+
+  let objs = match *geometry {
+    engine::resource_manager::Resource::Asset(ref m) => engine::exporter::obj::export(m),
+    _ => panic!("{:?} is not an asset, sorry.", geometry_path)
+  };
+
+  let dir = destination.clone();
+  fs::create_dir_all(&dir).unwrap();
+
+  for (path, obj) in objs {
+    let mut f = fs::File::create(dir.clone() + path.as_str()).unwrap();
+
+    f.write_all(obj.as_bytes()).unwrap();
   }
 }
